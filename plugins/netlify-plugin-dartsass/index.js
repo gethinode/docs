eb36e60640043831f3a597e57c114dc69eacadc1
mark <mark@dumay.nl>
2023-09-06 13:26:38 +0200

    Add initial Dart Sass plugin

---
+++ plugins/netlify-plugin-dartsass/index.js
@@ -0,0 +1,19 @@
+const { env } = require('node:process');
+
+module.exports = {
+    onPreBuild({ netlifyConfig }) {
+        let version = env.DART_SASS_VERSION || "1.66.1";
+
+        let newCommand = `
+            curl -LJO https://github.com/sass/dart-sass/releases/download/${version}/dart-sass-${version}-linux-x64.tar.gz && \
+            tar -xf dart-sass-${version}-linux-x64.tar.gz && \
+            rm dart-sass-${version}-linux-x64.tar.gz && \
+            export PATH=/opt/build/repo/dart-sass:$PATH`
+        
+
+        // Run a script prior to build command
+        netlifyConfig.build.command = netlifyConfig.build.command
+        ? `${newCommand} && ${netlifyConfig.build.command}`
+        : newCommand;
+    },
+};
